6b2f30da50347f86aa283a7ed869e159513922b5
brunoestivalli <bruno.estivalli@gmail.com>
2021-01-08 13:39:23 -0300

    Add routes and StepHeader

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,15 +1,15 @@
 import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
+import Routes from './Routes';
+
 
 
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-    </>
+    
+     
+      <Routes />
+    
   );
 }
 
